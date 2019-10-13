5305858a7fe733d0f7d441473791d9674531b872
James Jarvis <git@jamesjarvis.io>
2019-10-13 22:02:56 +0100

    now shows options for all event types and modules

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
-import { getAllLectureTypes } from '../../interface/api';
+import { getAllLectureTypes, getAllModules } from '../../interface/api';
+import './Filter.scss';
+import { Module } from '../../interface/db-types';
 
 interface SelectType {
   value: string;
@@ -20,20 +22,62 @@ function formatEventTypes(eventTypes: Array<string>): Array<SelectType> {
   return tempSelectTypes;
 }
 
+/**
+ * formatSubjectTypes formats the module database objects into the select types
+ *
+ * @param modules Information about all modules associated with the events
+ */
+function formatSubjectTypes(modules: Array<Module>): Array<GroupedSelectType> {
+  const tempSubTypes = new Array<GroupedSelectType>();
+  modules.forEach((mod: Module) => {
+    let found = false;
+    const newSelectType: SelectType = { value: mod.code, label: `${mod.code}: ${mod.name}` };
+    for (let i = 0; i < tempSubTypes.length; i += 1) {
+      if (tempSubTypes[i].label === mod.subject) {
+        found = true;
+        tempSubTypes[i].options.push(newSelectType);
+      }
+    }
+    if (!found) {
+      tempSubTypes.push({ label: mod.subject ? mod.subject : 'Other', options: new Array<SelectType>(newSelectType) });
+    }
+  });
+  return tempSubTypes;
+}
+
 const FilterView: React.FC = () => {
   const [eventTypes, setEventTypes] = useState();
-  const eventSelects = formatEventTypes(getAllLectureTypes());
+  const [availableEventTypes, setAvailableEventTypes] = useState();
+  useEffect(() => {
+    setAvailableEventTypes(formatEventTypes(getAllLectureTypes()));
+  }, []);
 
-  const handleChange = (selectedOption: any) => {
+  const handleTypeChange = (selectedOption: any) => {
     setEventTypes(selectedOption);
+  };
+
+  const [subjectTypes, setSubjectTypes] = useState();
+
+  const [availableSubjectTypes, setAvailableSubjectTypes] = useState();
+  useEffect(() => {
+    getAllModules().then((data) => {
+      if (data) {
+        setAvailableSubjectTypes(formatSubjectTypes(data));
+      }
+    });
+  }, []);
+
+
+  const handleSubjectChange = (selectedOption: any) => {
+    setSubjectTypes(selectedOption);
   };
 
   return (
     <div className="filterView">
       <span>Show me</span>
-      <Select value={eventTypes} onChange={handleChange} options={eventSelects} />
+      <Select value={eventTypes} onChange={handleTypeChange} options={availableEventTypes} className="selector" placeholder="All" isMulti />
       <span>events for</span>
-      <span>All subjects</span>
+      <Select value={subjectTypes} onChange={handleSubjectChange} options={availableSubjectTypes} className="selector" placeholder="All subjects" isMulti />
     </div>
   );
 };
