f77b5ad0eb7dfd222e745aa55de45e1d899975e5
jamesjarvis <jamesjarvis@users.noreply.github.com>
2019-10-18 20:38:58 +0000

    Automated deployment: Fri Oct 18 20:38:58 UTC 2019 ed10897699209b8a1b621fe9e4ae4e4ce2ea6bab

---
+++ precache-manifest.2aa1d1e49c562cfc6810922b58d62971.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96afb2bf43b22af1add961dfbdd4356d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cfa5234d791c300832e",
+    "url": "/static/css/main.013f705f.chunk.css"
+  },
+  {
+    "revision": "d5dba5291833da5469db",
+    "url": "/static/js/2.8ac269d2.chunk.js"
+  },
+  {
+    "revision": "1cfa5234d791c300832e",
+    "url": "/static/js/main.8f1d2cad.chunk.js"
+  },
+  {
+    "revision": "7360c531d1bf9570fb8b",
+    "url": "/static/js/runtime-main.17378048.js"
+  }
+]);
