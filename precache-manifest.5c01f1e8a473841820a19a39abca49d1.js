94eeb1b8298098cd652abcc7413959cd1d5cf7c4
jamesjarvis <jamesjarvis@users.noreply.github.com>
2019-10-19 18:13:32 +0000

    Automated deployment: Sat Oct 19 18:13:31 UTC 2019 ba20629d6918569e0f017cf6b8983b8e71191cfd

---
+++ precache-manifest.5c01f1e8a473841820a19a39abca49d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58908215b36967f99e36c17df6493992",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d728184f55c7a5d5fa9f",
+    "url": "/static/css/main.ff8f51b0.chunk.css"
+  },
+  {
+    "revision": "a6094b082c369d806630",
+    "url": "/static/js/2.bab3f0b4.chunk.js"
+  },
+  {
+    "revision": "d728184f55c7a5d5fa9f",
+    "url": "/static/js/main.00470c6f.chunk.js"
+  },
+  {
+    "revision": "7360c531d1bf9570fb8b",
+    "url": "/static/js/runtime-main.17378048.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e62ab8d1908e161782c708c37c0c2b63.js"
+  "/precache-manifest.5c01f1e8a473841820a19a39abca49d1.js"
 );
 
 self.addEventListener('message', (event) => {
