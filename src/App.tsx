b29a7acc02cfadfe205e7c854e6cab8301677cfd
James Jarvis <git@jamesjarvis.io>
2019-10-20 15:18:49 +0100

    💄 Added Material UI header and theme

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,25 @@ import {
   Redirect,
 } from 'react-router-dom';
 import './App.scss';
+import { MuiThemeProvider } from '@material-ui/core';
 import MainView from './pages/MainView/MainView';
+import Header from './components/Header/Header';
+import theme from './interface/theme';
 
 
 const App: React.FC = () => (
   <Router>
-    <div id="App">
-      <Switch>
-        <Route exact path="/" component={MainView} />
-        <Route path="*">
-          <Redirect to="/" />
-        </Route>
-      </Switch>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div id="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={MainView} />
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </div>
+    </MuiThemeProvider>
   </Router>
 );
 
---
+++ src/interface/theme.tsx
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: '#444',
+    },
+    secondary: {
+      main: '#3949ab',
+    },
+  },
+});
