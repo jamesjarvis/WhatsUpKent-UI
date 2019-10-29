19e95fbdaa9853b5af20afe9d8efd4642f3fd08e
jamesjarvis <jamesjarvis@users.noreply.github.com>
2019-10-29 12:46:19 +0000

    Automated deployment: Tue Oct 29 12:46:19 UTC 2019 3a0b11dddd827e7f17c4fbf65b2b6cf126563677

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff1e33c4b2a6bd7da91e143afb312c33.js"
+  "/precache-manifest.6a6f210fe3a0f9652a7cd575e5dc16a3.js"
 );
 
 self.addEventListener('message', (event) => {
