928e6a48ef7b09c69f94251265aaf378d08f980c
James Jarvis <git@jamesjarvis.io>
2019-10-29 12:01:17 +0000

    🥚 Add a little bit of information

--- src/pages/MainView/MainView.tsx
+++ src/pages/MainView/MainView.tsx
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react';
 import './MainView.scss';
+import Typography from '@material-ui/core/Typography';
 import FilterView from '../../components/Filter/Filter';
 import CalendarWrapper from '../../components/CalendarWrapper/CalendarWrapper';
 import FilterContext, { defaultFilter, reducer, init } from '../../components/Contexts/FilterContext';
@@ -9,6 +10,20 @@ const MainView: React.FC = () => {
 
   return (
     <div className="MainView">
+      <div className="info">
+        <Typography variant="body1">
+          See everything going on at the
+          <a
+            href="https://www.kent.ac.uk/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {' '}
+            University of Kent
+          </a>
+          , without nagging all of your mates.
+        </Typography>
+      </div>
       <FilterContext.Provider value={{ filterState, dispatch }}>
         <div className="filterHolder">
           <FilterView />
